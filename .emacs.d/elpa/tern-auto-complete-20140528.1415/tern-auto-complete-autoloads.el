ba1668b5a0637b7ab5fc27f51e98fa18dd7830f3
Shunsuke Takamiya <chang.shoon@gmail.com>
2014-09-05 22:51:07 +0100

    auto-complete installed

--- .emacs.d/elpa/tern-auto-complete-20140528.1415/tern-auto-complete-autoloads.el
+++
@@ -1,29 +0,0 @@
-;;; tern-auto-complete-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads (tern-ac-setup) "tern-auto-complete" "tern-auto-complete.el"
-;;;;;;  (21506 63511 0 0))
-;;; Generated autoloads from tern-auto-complete.el
-
-(autoload 'tern-ac-setup "tern-auto-complete" "\
-Setup auto-complete for tern-mode.
-
-\(fn)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("tern-auto-complete-pkg.el") (21506 63511
-;;;;;;  38372 0))
-
-;;;***
-
-(provide 'tern-auto-complete-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; tern-auto-complete-autoloads.el ends here
--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -397,12 +397,14 @@
 ;;----------------------
 ;; auto-compelte
 ;;----------------------
-;(when (require 'auto-complete-config nil t)
-;  (add-to-list 'ac-dictionary-directories "~/.emacs.d/elisp/ac-dict")
-;  (define-key ac-mode-map (kbd "M-TAB") 'auto-complete)
-;  (ac-config-default))
-;(require 'auto-complete-config)
-;(global-auto-complete-mode 1)
+(require 'auto-complete-config)
+(global-auto-complete-mode 1)
+;; (when (require 'auto-complete-config nil t)
+;;  (add-to-list 'ac-dictionary-directories "~/.emacs.d/elisp/ac-dict")
+;;  (define-key ac-mode-map (kbd "M-TAB") 'auto-complete)
+;;  (ac-config-default))
+;; (require 'auto-complete-config)
+
 
 
 ;;=============================================
