c1d6d6e9608c034c45a494f504beee1e540f7906
Shunsuke Takamiya <chang.shoon@gmail.com>
2014-10-02 17:23:09 +0100

    split into 3 windows

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -456,6 +456,12 @@
 ;;----------------------
 (require 'ipa)
 
+;;----------------------
+;; M-x which-func-mode
+;;----------------------
+
+
+
 ;;=============================================
 ;; 14 Create Documents
 ;;=============================================
@@ -609,7 +615,31 @@
 ;; (require 'smooth-scroll)
 ;; (smooth-scroll-mode t)
 
-
+;;----------------------------------
+;; split into 3 windows
+;;----------------------------------
+(defun split-window-vertically-n (num_wins)
+  (interactive "p")
+  (if (= num_wins 2)
+      (split-window-vertically)
+    (progn
+      (split-window-vertically
+       (- (window-height) (/ (window-height) num_wins)))
+      (split-window-vertically-n (- num_wins 1)))))
+(defun split-window-horizontally-n (num_wins)
+  (interactive "p")
+  (if (= num_wins 2)
+      (split-window-horizontally)
+    (progn
+      (split-window-horizontally
+       (- (window-width) (/ (window-width) num_wins)))
+      (split-window-horizontally-n (- num_wins 1)))))
+(global-set-key "\C-x@" '(lambda ()
+                           (interactive)
+                           (split-window-vertically-n 3)))
+(global-set-key "\C-x#" '(lambda ()
+                           (interactive)
+                           (split-window-horizontally-n 3)))
 
 ;;=============================================
 ;; For Terminal
