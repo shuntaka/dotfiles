fa63907739c9e66dfb6dacabca21d93f90341856
Shunsuke Takamiya <chang.shoon@gmail.com>
2014-11-19 12:24:42 +0000

    Revert "ctags setting modified"
    This reverts commit cf01bc2fd3cb54bf72a02935357f86fe644ede45.

--- .emacs.d/elpa/ctags/ctags-autoloads.el
+++
@@ -1,19 +0,0 @@
-;;; ctags-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil nil ("../../../../../.emacs.d/elpa/ctags-20110911.304/ctags-pkg.el"
-;;;;;;  "../../../../../.emacs.d/elpa/ctags-20110911.304/ctags.el")
-;;;;;;  (21612 28964 538440 0))
-
-;;;***
-
-(provide 'ctags-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; ctags-autoloads.el ends here
