66c4a416734add19fc20ebf2bafa1692a4c74b19
Shunsuke Takamiya <chang.shoon@gmail.com>
2015-01-20 12:45:48 +0000

    viewwer.el installed

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -10,6 +10,7 @@
 ;; 7. Search and Replace
 ;; 8. Making Emacs Even More Convinient
 ;; 9. External Program
+;; 11. View Mode
 ;; 13. For Programming
 ;; 14. Create Documents
 ;; Helm
@@ -736,6 +737,35 @@ org-modeなどで活用。"
 (global-set-key (kbd "s-d") 'e2wm:dp-edbi)
 
 ;;=============================================
+;; 11. view-mode
+;;=============================================
+(require 'view)
+;; less
+(define-key view-mode-map (kbd "N") 'View-search-last-regexp-backward)
+(define-key view-mode-map (kbd "?") 'View-search-regexp-backward)
+(define-key view-mode-map (kbd "G") 'View-goto-line-last)
+(define-key view-mode-map (kbd "b") 'View-scroll-page-backward)						   
+(define-key view-mode-map (kbd "f") 'View-scroll-page-forward)
+
+;; vi
+(define-key view-mode-map (kbd "h") 'backward-char)
+(define-key view-mode-map (kbd "j") 'next-line)
+(define-key view-mode-map (kbd "k") 'previous-line)
+(define-key view-mode-map (kbd "l") 'forward-char)
+(define-key view-mode-map (kbd "J") 'View-scroll-line-forward)
+(define-key view-mode-map (kbd "K") 'View-scroll-line-backward)
+
+;; bm.el
+(define-key view-mode-map (kbd "m") 'bm-toggle)
+(define-key view-mode-map (kbd "[") 'bm-previous)
+(define-key view-mode-map (kbd "]") 'bm-next)
+						   
+(require 'viewer)
+(setq viewer-modeline-color-unwritable "tomato")
+(setq viewer-modeline-color-view "orange")
+(viewer-change-modeline-color-setup)
+
+ ;;=============================================
 ;; 13. For Programming 
 ;;=============================================
 ;;----------------------------------------------
