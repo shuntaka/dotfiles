863b6f035da52d4e8f3c39e40b356d60f5b66d3d
Shunsuke Takamiya <chang.shoon@gmail.com>
2015-01-20 16:57:17 +0000

    setting for emacsclient

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -393,14 +393,13 @@
 ;;----------------------
 ;; emacsclient
 ;;----------------------
-;; emacsclient
-;; (server-start)
-;; (defun iconify-emacs-when-server-is-done ()
-;;   (unless server-clients (iconify-frame)))
+(server-start)
+(defun iconify-emacs-when-server-is-done ()
+  (unless server-clients (iconify-frame)))
 ;; 編集が終了したらEmacsをアイコン化する
-(add-hook 'server-done-hook 'iconify-emacs-when-server-is-done)
-;; ;;  C-x C-cに割り当てる
-;; (global-set-key (kbd "C-x C-c") 'server-edit)
+;; (add-hook 'server-done-hook 'iconify-emacs-when-server-is-done)
+;;  C-x C-cに割り当てる
+(global-set-key (kbd "C-x C-c") 'server-edit)
 ;; M-x exitでEmacsを終了できるようにする
 (defalias 'exit 'save-buffers-kill-emacs)
 
