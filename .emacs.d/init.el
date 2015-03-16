4364fe31b5d0b0c77e7fa9fce5edcf773dd0f2bc
Shunsuke Takamiya <chang.shoon@gmail.com>
2015-03-16 00:47:32 +0000

    helm setting 20150316

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -980,7 +980,7 @@ For example, type \\[event-apply-meta-control-modifier] % to enter Meta-Control-
 
 (global-set-key (kbd "C-;") 'helm-for-files)
 (global-set-key(kbd "C-x b") 'helm-mini)
-;; (define-key global-map (kbd "M-x")     'helm-M-x)
+(define-key global-map (kbd "M-x")     'helm-M-x)
 (define-key global-map (kbd "C-x C-f") 'helm-find-files)
 (define-key global-map (kbd "C-x C-r") 'helm-recentf)
 (define-key global-map (kbd "M-y")     'helm-show-kill-ring)
