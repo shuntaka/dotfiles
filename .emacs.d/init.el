e1e3e0d770398acf8026f0d70f43d5fe53320dc6
Shunsuke Takamiya <chang.shoon@gmail.com>
2015-04-30 01:34:50 +0100

    enable helm fine-file

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1189,12 +1189,12 @@ For example, type \\[event-apply-meta-control-modifier] % to enter Meta-Control-
 (global-set-key (kbd "C-;") 'helm-for-files)
 (global-set-key(kbd "C-x b") 'helm-mini)
 (define-key global-map (kbd "M-x")     'helm-M-x)
-;; (define-key global-map (kbd "C-x C-f") 'helm-find-files)
+(define-key global-map (kbd "C-x C-f") 'helm-find-files)
 (define-key global-map (kbd "C-x C-r") 'helm-recentf)
 (define-key global-map (kbd "M-y")     'helm-show-kill-ring)
 (define-key global-map (kbd "C-c i")   'helm-imenu)
 ;; (define-key global-map (kbd "C-x b")   'helm-buffers-list)
-;; (define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind tab to do persistent action
+(define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind tab to do persistent action
 (global-set-key (kbd "C-c s") 'helm-ag)
 ;; (global-set-key (kbd "!C-c y") 'helm-show-kill-ring)e
 
