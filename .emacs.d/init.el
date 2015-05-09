81f85a4ff2ce918391954792b085f3fc08b60771
Shunsuke Takamiya <chang.shoon@gmail.com>
2015-05-09 15:49:49 +0100

    powerline

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1396,7 +1396,7 @@ Otherwise goto the end of minibuffer."
 ;;----------------------
 ;; color theme
 ;;----------------------
-(load-theme 'zenburn t)
+;; (load-theme 'zenburn t)
 
 ;; (when (require 'color-theme nil t)
 ;; (color-theme-initialize))
@@ -1438,6 +1438,175 @@ Otherwise goto the end of minibuffer."
 ;; Background Color for Region
 ;;----------------------------------------------
 (set-face-background 'region "darkgreen")
+
+;;----------------------------------------------
+;; powerline colors
+;;http://blog.shibayu36.org/entry/2014/02/11/160945
+;;----------------------------------------------
+;;; I must require these two elisps before loading powerline...
+(require 'elscreen)
+(require 'smartrep)
+
+(require 'powerline)
+
+(set-face-attribute 'mode-line nil
+                    :foreground "#fff"
+                    :background "#FF0066"
+                    :box nil)
+
+(set-face-attribute 'powerline-active1 nil
+                    :foreground "#fff"
+                    :background "#FF6699"
+                    :inherit 'mode-line)
+
+(set-face-attribute 'powerline-active2 nil
+                    :foreground "#000"
+                    :background "#ffaeb9"
+                    :inherit 'mode-line)
+
+(powerline-default-theme)
+
+;;; modeの名前を自分で再定義
+(defvar mode-line-cleaner-alist
+  '( ;; For minor-mode, first char is 'space'
+    (flymake-mode . " Fm")
+    (paredit-mode . "")
+    (eldoc-mode . "")
+    (abbrev-mode . "")
+    (undo-tree-mode . "")
+    (git-gutter-mode . "")
+    (anzu-mode . "")
+    (yas-minor-mode . "")
+    (guide-key-mode . "")
+
+    ;; Major modes
+    (fundamental-mode . "Fund")
+    (dired-mode . "Dir")
+    (lisp-interaction-mode . "Li")
+    (cperl-mode . "Pl")
+    (python-mode . "Py")
+    (ruby-mode   . "Rb")
+    (emacs-lisp-mode . "El")
+    (markdown-mode . "Md")))
+
+(defun clean-mode-line ()
+  (interactive)
+  (loop for (mode . mode-str) in mode-line-cleaner-alist
+        do
+        (let ((old-mode-str (cdr (assq mode minor-mode-alist))))
+          (when old-mode-str
+            (setcar old-mode-str mode-str))
+          ;; major mode
+          (when (eq mode major-mode)
+            (setq mode-name mode-str)))))
+
+(add-hook 'after-change-major-mode-hook 'clean-mode-line)
+
+;;----------------------------------------------
+;; powerline simplify the display
+;;http://d.hatena.ne.jp/syohex/20130131/1359646452
+;;----------------------------------------------
+(defvar mode-line-cleaner-alist
+  '( ;; For minor-mode, first char is 'space'
+    (yas-minor-mode . " Ys")
+    (paredit-mode . " Pe")
+    (eldoc-mode . "")
+    (abbrev-mode . "")
+    (undo-tree-mode . " Ut")
+    (elisp-slime-nav-mode . " EN")
+    (helm-gtags-mode . " HG")
+    (flymake-mode . " Fm")
+    ;; Major modes
+    (lisp-interaction-mode . "Li")
+    (python-mode . "Py")
+    (ruby-mode   . "Rb")
+    (emacs-lisp-mode . "El")
+    (markdown-mode . "Md")))
+
+(defun clean-mode-line ()
+  (interactive)
+  (loop for (mode . mode-str) in mode-line-cleaner-alist
+        do
+        (let ((old-mode-str (cdr (assq mode minor-mode-alist))))
+          (when old-mode-str
+            (setcar old-mode-str mode-str))
+          ;; major mode
+          (when (eq mode major-mode)
+            (setq mode-name mode-str)))))
+
+(add-hook 'after-change-major-mode-hook 'clean-mode-line)
+
+
+;;----------------------------------------------
+;; powerline customize what info to show
+;; http://blog.shibayu36.org/entry/2014/04/01/094543
+;;----------------------------------------------
+(defun shorten-directory (dir max-length)
+  "Show up to `max-length' characters of a directory name `dir'."
+  (let ((path (reverse (split-string (abbreviate-file-name dir) "/")))
+        (output ""))
+    (when (and path (equal "" (car path)))
+      (setq path (cdr path)))
+    (while (and path (< (length output) (- max-length 4)))
+      (setq output (concat (car path) "/" output))
+      (setq path (cdr path)))
+    (when path
+      (setq output (concat ".../" output)))
+    output))
+
+(defun powerline-my-theme ()
+  "Setup the my mode-line."
+  (interactive)
+  (setq-default mode-line-format
+                '("%e"
+                  (:eval
+                   (let* ((active (powerline-selected-window-active))
+                          (mode-line (if active 'mode-line 'mode-line-inactive))
+                          (face1 (if active 'powerline-active1 'powerline-inactive1))
+                          (face2 (if active 'powerline-active2 'powerline-inactive2))
+                          (separator-left (intern (format "powerline-%s-%s"
+                                                          powerline-default-separator
+                                                          (car powerline-default-separator-dir))))
+                          (separator-right (intern (format "powerline-%s-%s"
+                                                           powerline-default-separator
+                                                           (cdr powerline-default-separator-dir))))
+                          (lhs (list (powerline-raw "%*" nil 'l)
+                                     (powerline-buffer-size nil 'l)
+                                     (powerline-raw mode-line-mule-info nil 'l)
+                                     ;;; !!! ここから書き換えた !!!
+                                     (powerline-raw
+                                      (shorten-directory default-directory 15)
+                                      nil 'l)
+                                     (powerline-buffer-id nil 'r)
+                                     ;;; !!! ここまで書き換えた !!!
+                                     (when (and (boundp 'which-func-mode) which-func-mode)
+                                       (powerline-raw which-func-format nil 'l))
+                                     (powerline-raw " ")
+                                     (funcall separator-left mode-line face1)
+                                     (when (boundp 'erc-modified-channels-object)
+                                       (powerline-raw erc-modified-channels-object face1 'l))
+                                     (powerline-major-mode face1 'l)
+                                     (powerline-process face1)
+                                     (powerline-minor-modes face1 'l)
+                                     (powerline-narrow face1 'l)
+                                     (powerline-raw " " face1)
+                                     (funcall separator-left face1 face2)
+                                     (powerline-vc face2 'r)))
+                          (rhs (list (powerline-raw global-mode-string face2 'r)
+                                     (funcall separator-right face2 face1)
+                                     (powerline-raw "%4l" face1 'l)
+                                     (powerline-raw ":" face1 'l)
+                                     (powerline-raw "%3c" face1 'r)
+                                     (funcall separator-right face1 mode-line)
+                                     (powerline-raw " ")
+                                     (powerline-raw "%6p" nil 'r)
+                                     (powerline-hud face2 face1))))
+                     (concat (powerline-render lhs)
+                             (powerline-fill face2 (powerline-width rhs))
+                             (powerline-render rhs)))))))
+
+
+
 
 
 
