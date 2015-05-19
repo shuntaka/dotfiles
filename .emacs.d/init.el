d9e28c8c9d5bb58994d50694dca93f5538810565
Shunsuke Takamiya <chang.shoon@gmail.com>
2015-05-19 05:37:40 +0100

    disable highlighting between paren

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -197,9 +197,10 @@
 ;;----------------------
 (setq show-paren-delay 0)
 (show-paren-mode t)
-(setq show-paren-style 'expression)
+;; (setq show-paren-style 'expression)
 (set-face-background 'show-paren-match-face nil)
 (set-face-underline-p 'show-paren-match-face "yellow")
+
 
 ;;----------------------------------------------
 ;; ターミナル以外はツーバーとスクロールバーを消す
