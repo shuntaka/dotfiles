20cea14287508ef2b5156636ca9c0a2fd18ca297
Shunsuke Takamiya <chang.shoon@gmail.com>
2015-05-29 12:33:56 +0100

    wgrep

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -770,6 +770,63 @@ org-modeなどで活用。"
 (global-ace-isearch-mode 1)
 ;; '(ace-isearch-input-idle-delay 0.1)
 
+;;----------------------------------------------
+;; emacs ag.el
+;;----------------------------------------------
+;;http://rubikitch.com/2014/09/12/ag/
+(setq ag-highlight-search t)
+
+;;http://kotatu.org/blog/2013/12/18/emacs-aag-wgrep-for-code-grep-search/
+;; ag
+;; ag(The Silver Searcher)コマンドを以下からインストール:
+;;     http://github.com/ggreer/the_silver_searcher#installation
+;; ag.elとwgrep-ag.elをlist-packageでMelpaなどからインストールしておく
+(require 'ag)
+(custom-set-variables
+ '(ag-highlight-search t)  ; 検索結果の中の検索語をハイライトする
+ '(ag-reuse-window 'nil)   ; 現在のウィンドウを検索結果表示に使う
+ '(ag-reuse-buffers 'nil)) ; 現在のバッファを検索結果表示に使う
+(require 'wgrep-ag)
+(autoload 'wgrep-ag-setup "wgrep-ag")
+(add-hook 'ag-mode-hook 'wgrep-ag-setup)
+;; agの検索結果バッファで"r"で編集モードに。
+;; C-x C-sで保存して終了、C-x C-kで保存せずに終了
+(define-key ag-mode-map (kbd "r") 'wgrep-change-to-wgrep-mode)
+;; キーバインドを適当につけておくと便利。"\C-xg"とか
+;; (global-set-key [(super m)] 'ag)
+(global-set-key (kbd "M-m") 'ag)
+;; ag開いたらagのバッファに移動するには以下をつかう
+(defun my/filter (condp lst)
+  (delq nil
+        (mapcar (lambda (x) (and (funcall condp x) x)) lst)))
+(defun my/get-buffer-window-list-regexp (regexp)
+  "Return list of windows whose buffer name matches regexp."
+  (my/filter #'(lambda (window)
+              (string-match regexp
+               (buffer-name (window-buffer window))))
+          (window-list)))
+(global-set-key [(super m)]
+                #'(lambda ()
+                    (interactive)
+                    (call-interactively 'ag)
+                    (select-window ; select ag buffer
+                     (car (my/get-buffer-window-list-regexp "^\\*ag ")))))
+
+;;----------------------------------------------
+;; wgrep
+;; http://rubikitch.com/2014/09/13/wgrep/
+;;----------------------------------------------
+;; 1. M-x ag
+;; 2. eでwgrep-modeにする
+;; 3. *grep*バッファを編集する
+;; 4. C-c C-c (C-x C-s)でファイルに反映！
+
+;;; eでwgrepモードにする
+(setf wgrep-enable-key "e")
+;;; wgrep終了時にバッファを保存
+(setq wgrep-auto-save-buffer t)
+;;; read-only bufferにも変更を適用する
+(setq wgrep-change-readonly-file t)
 
 ;;----------------------------------------------
 ;; foreign-regexp.el
@@ -933,47 +990,6 @@ org-modeなどで活用。"
 (autoload 'e2wm:dp-edbi "e2wm-edbi" nil t)
 (global-set-key (kbd "s-d") 'e2wm:dp-edbi)
 
-;;----------------------------------------------
-;; emacs ag.el
-;;----------------------------------------------
-;;http://rubikitch.com/2014/09/12/ag/
-(setq ag-highlight-search t)
-
-;;http://kotatu.org/blog/2013/12/18/emacs-aag-wgrep-for-code-grep-search/
-;; ag
-;; ag(The Silver Searcher)コマンドを以下からインストール:
-;;     http://github.com/ggreer/the_silver_searcher#installation
-;; ag.elとwgrep-ag.elをlist-packageでMelpaなどからインストールしておく
-(require 'ag)
-(custom-set-variables
- '(ag-highlight-search t)  ; 検索結果の中の検索語をハイライトする
- '(ag-reuse-window 'nil)   ; 現在のウィンドウを検索結果表示に使う
- '(ag-reuse-buffers 'nil)) ; 現在のバッファを検索結果表示に使う
-(require 'wgrep-ag)
-(autoload 'wgrep-ag-setup "wgrep-ag")
-(add-hook 'ag-mode-hook 'wgrep-ag-setup)
-;; agの検索結果バッファで"r"で編集モードに。
-;; C-x C-sで保存して終了、C-x C-kで保存せずに終了
-(define-key ag-mode-map (kbd "r") 'wgrep-change-to-wgrep-mode)
-;; キーバインドを適当につけておくと便利。"\C-xg"とか
-;; (global-set-key [(super m)] 'ag)
-(global-set-key (kbd "M-m") 'ag)
-;; ag開いたらagのバッファに移動するには以下をつかう
-(defun my/filter (condp lst)
-  (delq nil
-        (mapcar (lambda (x) (and (funcall condp x) x)) lst)))
-(defun my/get-buffer-window-list-regexp (regexp)
-  "Return list of windows whose buffer name matches regexp."
-  (my/filter #'(lambda (window)
-              (string-match regexp
-               (buffer-name (window-buffer window))))
-          (window-list)))
-(global-set-key [(super m)]
-                #'(lambda ()
-                    (interactive)
-                    (call-interactively 'ag)
-                    (select-window ; select ag buffer
-                     (car (my/get-buffer-window-list-regexp "^\\*ag ")))))
 
 ;;----------------------------------------------
 ;; iterm2
