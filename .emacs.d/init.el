6e3b1da3104908b54a715d86ec7523ac0406e27f
Shunsuke Takamiya <chang.shoon@gmail.com>
2015-06-26 00:53:41 +0100

    web-mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -237,11 +237,6 @@
           (lambda ()
             (setq-default tab-width 2)))
 
-;; for web-mode
-(add-hook 'web-mode-hook
-          (lambda ()
-            (setq-default tab-width 2)))
-
 ;; for javascript
 (add-hook 'js2-mode-hook
           (lambda ()
@@ -1918,13 +1913,96 @@ Otherwise goto the end of minibuffer."
 ;;=============================================
 ;; For HTML and CSS
 ;;=============================================
+;;----------------------------------------------
+;; register the files to open with web-mod
+;;----------------------------------------------
+(setq auto-mode-alist
+      (append '(
+                ("\\.\\(html\\|xhtml\\|shtml\\|tpl\\|tt\\|ms\\)\\'" . web-mode)
+                ("\\.php\\'" . php-mode)
+                )
+              auto-mode-alist))
 
 ;;----------------------------------------------
-;; disable tab for html
+;; web-modeの設定
+;; http://yanmoo.blogspot.co.uk/2013/06/html5web-mode.html
 ;;----------------------------------------------
-(add-hook 'html-mode-hook
+(require 'web-mode)
+(defun web-mode-hook ()
+  "Hooks for Web mode."
+  ;; 変更日時の自動修正
+  (setq time-stamp-line-limit -200)
+  (if (not (memq 'time-stamp write-file-hooks))
+      (setq write-file-hooks
+            (cons 'time-stamp write-file-hooks)))
+  (setq time-stamp-format " %3a %3b %02d %02H:%02M:%02S %:y %Z")
+  (setq time-stamp-start "Last modified:")
+  (setq time-stamp-end "$")
+  ;; web-modeの設定
+  (setq web-mode-markup-indent-offset 2) ;; html indent
+  (setq web-mode-css-indent-offset 2)    ;; css indent
+  (setq web-mode-code-indent-offset 4)   ;; script indent(js,php,etc..)
+  ;; htmlの内容をインデント
+  ;; TEXTAREA等の中身をインデントすると副作用が起こったりするので
+  ;; デフォルトではインデントしない
+  ;;(setq web-mode-indent-style 2)
+  ;; コメントのスタイル
+  ;;   1:htmlのコメントスタイル(default)
+  ;;   2:テンプレートエンジンのコメントスタイル
+  ;;      (Ex. {# django comment #},{* smarty comment *},{{-- blade comment --}})
+  (setq web-mode-comment-style 2)
+  ;; 終了タグの自動補完をしない
+  ;;(setq web-mode-disable-auto-pairing t)
+  ;; color:#ff0000;等とした場合に指定した色をbgに表示しない
+  ;;(setq web-mode-disable-css-colorization t)
+  ;;css,js,php,etc..の範囲をbg色で表示
+  ;; (setq web-mode-enable-block-faces t)
+  ;; (custom-set-faces
+  ;;  '(web-mode-server-face
+  ;;    ((t (:background "grey"))))                  ; template Blockの背景色
+  ;;  '(web-mode-css-face
+  ;;    ((t (:background "grey18"))))                ; CSS Blockの背景色
+  ;;  '(web-mode-javascript-face
+  ;;    ((t (:background "grey36"))))                ; javascript Blockの背景色
+  ;;  )
+  ;;(setq web-mode-enable-heredoc-fontification t)
+)
+(add-hook 'web-mode-hook  'web-mode-hook)
+;; 色の設定
+(custom-set-faces
+ '(web-mode-doctype-face
+   ((t (:foreground "#82AE46"))))                          ; doctype
+ '(web-mode-html-tag-face
+   ((t (:foreground "#E6B422" :weight bold))))             ; 要素名
+ '(web-mode-html-attr-name-face
+   ((t (:foreground "#C97586"))))                          ; 属性名など
+ '(web-mode-html-attr-value-face
+   ((t (:foreground "#82AE46"))))                          ; 属性値
+ '(web-mode-comment-face
+   ((t (:foreground "#D9333F"))))                          ; コメント
+ '(web-mode-server-comment-face
+   ((t (:foreground "#D9333F"))))                          ; コメント
+ '(web-mode-css-rule-face
+   ((t (:foreground "#A0D8EF"))))                          ; cssのタグ
+ '(web-mode-css-pseudo-class-face
+   ((t (:foreground "#FF7F00"))))                          ; css 疑似クラス
+ '(web-mode-css-at-rule-face
+   ((t (:foreground "#FF7F00"))))                          ; cssのタグ
+)
+;;----------------------------------------------
+;; disable tab for web-mode
+;;----------------------------------------------
+(add-hook 'web-mode-hook
           (lambda ()
             (setq-default indent-tabs-mode nil)))
+
+;;----------------------------------------------
+;; tab width
+;;----------------------------------------------
+;; for-web-mode
+(add-hook 'web-mode-hook
+          (lambda ()
+            (setq-default tab-width 2)))
 
 ;;----------------------------------------------
 ;; rainbow-mode
