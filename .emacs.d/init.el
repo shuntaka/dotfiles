f30421b0aeb6dc960d6eda4d34a42616eab170e6
Shunsuke Takamiya <shun@Shunsuke-no-MacBook-Pro.local>
2015-11-13 10:52:15 +0900

    tune idle timer for highlight line

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -198,7 +198,7 @@
   (let ((global-hl-line-mode t))
     (global-hl-line-highlight)))
 (setq global-hl-line-timer
-      (run-with-idle-timer 0.1 t 'global-hl-line-timer-function))
+      (run-with-idle-timer 0.03 t 'global-hl-line-timer-function))
 ;; (cancel-timer global-hl-line-timer)
 
 ;;----------------------
