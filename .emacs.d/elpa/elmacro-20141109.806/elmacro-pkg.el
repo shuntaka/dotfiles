b9da06ce0d60b5c91fcd51026001e3d36442c2e7
Shunsuke Takamiya <shun@Shunsuke-no-MacBook-Pro.local>
2015-12-25 16:26:06 +0900

    elmacro package

---
+++ .emacs.d/elpa/elmacro-20141109.806/elmacro-pkg.el
@@ -0,0 +1,1 @@
+(define-package "elmacro" "20141109.806" "Convert keyboard macros to elisp" '((s "1.9.0") (dash "1.5.0")) :url "https://github.com/Silex/elmacro" :keywords '("macro" "elisp" "convenience"))
--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1395,6 +1395,23 @@ For example, type \\[event-apply-meta-control-modifier] % to enter Meta-Control-
 
 (global-set-key (kbd "C-c a") 'org-agenda)
 
+;;----------------------------------------------
+;; short cut for task management by elmacro
+;;----------------------------------------------
+;; move a task item to the end of COMPLETED section
+(defun move-task-item-to-COMPLETED ()
+  "move the task item to the end of COMPLETED section"
+  (interactive)
+  (org-seq-home)
+  (kill-line nil)
+  (forward-page 1)
+  (newline)
+  (yank nil)
+  (point-undo)
+  (org-delete-char 1))
+
+(global-set-key (kbd "C-c k") 'move-task-item-to-COMPLETED)
+
 ;;----------------------
 ;;; howm from Otake Tomoy's  emacs Jissen Nyumon, p148
 ;;----------------------
