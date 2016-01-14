74788051a2eca3de6fb5c6d6d3854b6653a23459
Shunsuke Takamiya <shun@Shunsuke-no-MacBook-Pro.local>
2016-01-14 14:56:33 +0900

    update todo files

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1433,16 +1433,14 @@ For example, type \\[event-apply-meta-control-modifier] % to enter Meta-Control-
 ;; http://qiita.com/takaxp/items/0b717ad1d0488b74429d
 ;;----------------------------------------------
 (setq org-agenda-files
-      '("~/Dropbox/Manage/ToDo/tempo_todo.org"
-        "~/Dropbox/Manage/Todo/jimu_todo.org"
-        "~/Dropbox/Manage/Todo/kai開発移管_todo.org"
-        "~/Dropbox/Manage/Todo/kan環境構築_todo.org"
-        "~/Dropbox/Manage/Todo/VMP_troubleshooting_todo.org"
-        "~/Dropbox/Manage/Todo/SRP_KT_todo.org"
-        "~/Dropbox/Manage/ToDo/schedule2015.org"
-        "~/Dropbox/Manage/ToDo/India_business_trip_todo.org"
+      '("~/Dropbox/Manage/ToDo/Office/office_ToDo_INBOX.org"
+        "~/Dropbox/Manage/Todo/Office/jimu_todo.org"
+        "~/Dropbox/Manage/Todo/Office/kai開発移管_todo.org"
+        "~/Dropbox/Manage/Todo/Office/kan環境構築_todo.org"
+        "~/Dropbox/Manage/Todo/Office/VMP_troubleshooting_todo.org"
+        "~/Dropbox/Manage/Todo/Office/SRP_KT_todo.org"
+        "~/Dropbox/Manage/ToDo/Office/schedule2015.org"
         ))
-
 (global-set-key (kbd "C-c a") 'org-agenda)
 
 
