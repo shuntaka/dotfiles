436f8c44488286745506f24ebaaec37abfeef840
Shunsuke Takamiya <shun@Shunsuke-no-MacBook-Pro.local>
2016-02-22 10:56:34 +0900

    install multiple cursor and smart-paren

---
+++ .emacs.d/elpa/paren-completer-20150711.1523/paren-completer-pkg.el
@@ -0,0 +1,1 @@
+(define-package "paren-completer" "20150711.1523" "Automatically, language agnostically, fill in delimiters." '((emacs "24.3")) :url "https://github.com/MatthewBregg/paren-completer" :keywords '("convenience"))
--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1046,13 +1046,44 @@
 (setq save-abbrevs nil)
 
 ;;----------------------------------------------
+;; paren-completer
+;;----------------------------------------------
+(global-set-key (kbd "M-)") 'paren-completer-add-single-delimiter)
+
+
+;;----------------------------------------------
+;; multiple-cursors
+;; http://rubikitch.com/2014/11/10/multiple-cursors/
+;;----------------------------------------------
+(require 'multiple-cursors)
+(defun mc/edit-lines-or-string-rectangle (s e)
+  "C-x r tで同じ桁の場合にmc/edit-lines (C-u M-x mc/mark-all-dwim)"
+  (interactive "r")
+  (if (eq (save-excursion (goto-char s) (current-column))
+          (save-excursion (goto-char e) (current-column)))
+      (call-interactively 'mc/edit-lines)
+    (call-interactively 'string-rectangle)))
+(global-set-key (kbd "C-x r t") 'mc/edit-lines-or-string-rectangle)
+
+(defun mc/mark-all-dwim-or-mark-sexp (arg)
+  "C-u C-M-SPCでmc/mark-all-dwim, C-u C-u C-M-SPCでC-u M-x mc/mark-all-dwim"
+  (interactive "p")
+  (cl-case arg
+    (16 (mc/mark-all-dwim t))
+    (4 (mc/mark-all-dwim nil))
+    (1 (mark-sexp 1))))
+(global-set-key (kbd "C-M-SPC") 'mc/mark-all-dwim-or-mark-sexp)
+
+
+;;----------------------------------------------
 ;; mark current word with thingopt.el
 ;;----------------------------------------------
 ;; using thingopt.el mark-word
 (defun shun-mark-current-word ()
     "Change me!"
   (interactive)
-  (mark-thing "word"))
+  (mark-thing "word")
+  (exchange-point-and-mark))
 (define-key global-map (kbd "C-M-f") 'shun-mark-current-word)
 
 ;;----------------------------------------------
